6cf86a66ea4119462034b19825211223cdc9b141
Nils Mueller <nmueller@physnet.uni-hamburg.de>
2024-05-22 14:52:12 +0200

    LED hysterisis

--- main/LedControl.h
+++ main/LedControl.h
@@ -62,16 +62,39 @@ static f32 ComputeSteeringAngle(const f32 velocity, const f32 roll)
     return steering_angle;
 }
 
+static u32 ComputeCorrectSegment(
+    const f32 value, const u32 prev_index,
+    const f32 region_width, const f32 hysteresis)
+{
+    Assert(value >= 0.0f, "Value cannot be negative");
+    const u32 current_index = static_cast<u32>(value / region_width);
+    if ((prev_index == -1) ||         // was not initialized before
+        (prev_index == current_index) // segment has not changed
+        ) return current_index;
+    const u32 segment_distance = abs(current_index - prev_index);
+    if (segment_distance > 1) return current_index;
+    const f32 normalized_hysteresis = hysteresis / region_width;
+    const f32 normalized_velocity = velocity / region_width;
+    const f32 normalized_distance = abs(normalized_velocity - round(normalized_velocity));
+    if (normalized_distance > normalized_hysteresis) return current_index;
+    return prev_index;
+}
+
 static void UpdateMatrix(const f32 velocity, const f32 roll)
 // TODO(Nils): Hysteresis missing
 // make it such that the roll is 0 if the bike goes straight
 {
-    static constexpr f32 critical_velocity = 9.0f;
-    static constexpr f32 critical_angle = DegToRad(13.0);
+    static constexpr f32 critical_velocity = 9.0f / 3.6f;  // m/s
+    static constexpr f32 critical_angle    = DegToRad(13.0);
+    static constexpr f32 hysteresis_velocity = (2.0f / 3.6f);
+    static constexpr f32 hysteresis_angle    = DegToRad(3.0f);
+    static u32 row_index    = -1;
+    static u32 column_index = -1;
     // steering angle of the bicycle
     const f32 steering_angle = ComputeSteeringAngle(velocity, roll);
-    const u32 row_index = (u32)(velocity / critical_velocity);
-    const u32 column_index = (u32)(ClampZero(2.0f * critical_angle + steering_angle) / critical_angle);
+    const f32 shifted_angle = ClampZero(3.0f * critical_angle + steering_angle);
+    row_index    = ComputeCorrectSegment(velocity    ,  row_index   , critical_velocity, hysteresis_velocity);
+    column_index = ComputeCorrectSegment(shifted_angle, column_index, critical_angle   , hysteresis_angle);
     SetMatrixState(row_index, column_index);
 }
 
